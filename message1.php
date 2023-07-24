f898422cc43ddbe050548a50cd116b9b87b0184b
Prat55 <pratikdesai9059@gmail.com>
2023-07-24 19:00:44 +0530

    changes

--- message1.php
+++ message1.php
@@ -1,15 +1,15 @@
 <?php
   $name = htmlspecialchars($_POST['name']);
-  $cname = htmlspecialchars($_POST['cname']);
-  $location = htmlspecialchars($_POST['location']);
   $email = htmlspecialchars($_POST['email']);
-  $phone = htmlspecialchars($_POST['phone']);
+  $phone = htmlspecialchars($_POST['number']);
+  $location = htmlspecialchars($_POST['location']);
+  $message = htmlspecialchars($_POST['message']);
 
-  if(!empty($email) && !empty($phone) && !empty($location) && !empty($name) && !empty($cname)){
+  if(!empty($email) && !empty($phone) && !empty($location) && !empty($name) && !empty($message)){
     if(filter_var($email, FILTER_VALIDATE_EMAIL)){
       $receiver = "pratikdesai4841@gmail.com"; //enter that email address where you want to receive all messages
       $subject = "From: $name <$email>";
-      $body = "Name: $name\nPhone: $phone\nEmail: $email\nCompany Name: $cname\nLocation: $location\n\nRegard,\n$name";
+      $body = "Name: $name\nPhone: $phone\nEmail: $email\nLocation: $location\nMessage: $message\n\nRegard,\n$name";
       $sender = "From: $email";
       if(mail($receiver, $subject, $body, $sender)){
         header("Location: thankyou.html");
